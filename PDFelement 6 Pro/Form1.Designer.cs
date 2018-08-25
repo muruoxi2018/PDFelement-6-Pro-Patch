2bef1fb99d0d928b8bd76eaa6e55f2b5a9123f25
MURUOXI\慕若曦 <13993481533@163.com>
2018-08-25 16:55:19 +0800

    v6.4

---
+++ PDFelement 6 Pro/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PDFelement_6_Pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gotham-Book", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(182, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ver：6.4.2.3104";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Gotham-Book", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(35, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 36);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "屏蔽Hosts";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Gotham-Book", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(197, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 36);
+            this.button2.TabIndex = 2;
+            this.button2.TabStop = false;
+            this.button2.Text = "应用设置";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Gotham-Book", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(359, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 36);
+            this.button3.TabIndex = 3;
+            this.button3.TabStop = false;
+            this.button3.Text = "恢复Hosts";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gotham-Book", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(490, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "x";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(520, 300);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PDFelement 6 Pro/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace PDFelement_6_Pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        bool beginMove = false;//初始化鼠标位置  
+        int currentXPosition;
+        int currentYPosition;
+
+        private void Label2_Click(object sender, EventArgs e) => Close();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                beginMove = true;
+                currentXPosition = MousePosition.X;//鼠标的x坐标为当前窗体左上角x坐标  
+                currentYPosition = MousePosition.Y;//鼠标的y坐标为当前窗体左上角y坐标  
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (beginMove)
+            {
+                this.Left += MousePosition.X - currentXPosition;//根据鼠标x坐标确定窗体的左边坐标x  
+                this.Top += MousePosition.Y - currentYPosition;//根据鼠标的y坐标窗体的顶部，即Y坐标  
+                currentXPosition = MousePosition.X;
+                currentYPosition = MousePosition.Y;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                currentXPosition = 0; //设置初始状态  
+                currentYPosition = 0;
+                beginMove = false;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            string hostspath = @"C:\WINDOWS\system32\drivers\etc\hosts";
+            File.SetAttributes(hostspath, FileAttributes.Normal);
+            FileStream hosts = new FileStream(hostspath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(hosts);
+            sw.WriteLine("127.0.0.1    platform.wondershare.com");
+            sw.WriteLine("127.0.0.1    support.wondershare.net");
+            sw.WriteLine("127.0.0.1    cbs.wondershare.com");
+            sw.WriteLine("127.0.0.1    www.wondershare.net");
+            sw.Close();
+            hosts.Close();
+            MessageBox.Show("屏蔽成功！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Process.Start("https://www.muruoxi.com/");
+            }
+            catch
+            {
+                MessageBox.Show("屏蔽失败！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+            RegistryKey reg = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32);
+           
+                try { 
+                if (Environment.Is64BitOperatingSystem) {
+                    var.path = reg.OpenSubKey(@"SOFTWARE\WOW6432Node\Wondershare\Wondershare PDFelement 6 Pro").GetValue("appsetuppath").ToString();
+                }
+                else
+                {
+                    var.path = reg.OpenSubKey(@"SOFTWARE\Wondershare\Wondershare PDFelement 6 Pro").GetValue("appsetuppath").ToString();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("请先安装PDFelement 6 Pro产品！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                reg.Close();
+            }
+
+            System.Reflection.Assembly asm = System.Reflection.Assembly.GetEntryAssembly();
+            FileStream fs;
+            BinaryWriter bw;
+            byte[] temp;
+
+            System.IO.Stream dll1 = asm.GetManifestResourceStream("PDFelement_6_Pro.Resources.PEStudio.PDFElement.Base.dll");
+            temp = new byte[dll1.Length];
+            dll1.Read(temp, 0, temp.Length);
+            dll1.Seek(0, SeekOrigin.Begin);
+            
+            try
+            {
+                fs = new FileStream(var.path + @"\PEStudio.PDFElement.Base.dll", FileMode.Create);
+                bw = new BinaryWriter(fs);
+                bw.Write(temp);
+                fs.Close();
+                bw.Close();
+            }
+            catch
+            {
+                MessageBox.Show("设置失败！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            System.IO.Stream dll2 = asm.GetManifestResourceStream("PDFelement_6_Pro.Resources.ProductAuthor.dll");
+            temp = new byte[dll2.Length];
+            dll2.Read(temp, 0, temp.Length);
+            dll2.Seek(0, SeekOrigin.Begin);
+            
+            try
+            {
+                fs = new FileStream(var.path + @"\ProductAuthor.dll", FileMode.Create);
+                bw = new BinaryWriter(fs);
+                bw.Write(temp);
+                fs.Close();
+                bw.Close();
+            }
+            catch
+            {
+                MessageBox.Show("设置失败！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            } 
+
+            MessageBox.Show("设置成功！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Process.Start("https://www.muruoxi.com/");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            string hostspath = @"C:\WINDOWS\system32\drivers\etc\hosts";
+            File.SetAttributes(hostspath, FileAttributes.Normal);
+            FileStream hosts = new FileStream(hostspath, FileMode.Open,FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(hosts,Encoding.GetEncoding("gb2312"));
+            StreamReader sr = new StreamReader(hosts,Encoding.GetEncoding("gb2312"));
+            string contents = sr.ReadToEnd();
+            contents = Regex.Replace(contents, @"127.0.0.1\s*platform.wondershare.com", "", RegexOptions.IgnoreCase);
+            contents = Regex.Replace(contents, @"127.0.0.1\s*support.wondershare.net", "", RegexOptions.IgnoreCase);
+            contents = Regex.Replace(contents, @"127.0.0.1\s*cbs.wondershare.com", "", RegexOptions.IgnoreCase);
+            contents = Regex.Replace(contents, @"127.0.0.1\s*www.wondershare.net", "", RegexOptions.IgnoreCase);
+            hosts.SetLength(0);
+            sw.Write(contents);
+            sw.Close();
+            hosts.Close();
+            MessageBox.Show("恢复成功！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Process.Start("https://www.muruoxi.com/");
+            }
+            catch
+            {
+                MessageBox.Show("恢复失败！", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        public class var {
+            public static string path;
+        }
+        
+    }
+}
